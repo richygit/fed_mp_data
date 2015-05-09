fbea459c31f2df91f624e224fbdaeb5d5788c87e
richygit <richard@getup.org.au>
2015-05-09 23:30:39 +1000

    added webmock back in. added rakefile for travis building

--- spec/csv_scraper_spec.rb
+++ spec/csv_scraper_spec.rb
@@ -4,7 +4,15 @@ require 'open-uri'
 describe CsvScraper do
   it "can download the CSV file" do
     VCR.turned_off do
+      WebMock.allow_net_connect!
       Net::HTTP.start(CsvScraper::CSV_HOST, 80) {|http| expect(http.head(CsvScraper::CSV_PATH).code).to eq "200" }
     end
+  end
+
+  it "scrapes MPs details" do
+    #spot check on various details from CSV file
+  end
+
+  it "scrapes the right number of MPs" do
   end
 end
