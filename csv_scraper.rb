cebf52f93864416c0a94514f45e9dd7bc0db7c32
richygit <richard@getup.org.au>
2015-05-18 22:42:41 +1000

    updated csv scraper for senators

--- csv_scraper.rb
+++ csv_scraper.rb
@@ -4,29 +4,41 @@ require 'open-uri'
 #get detailed info of MPs including titles and cabinet positions
 class CsvScraper
   CSV_HOST = 'www.aph.gov.au'
-  CSV_PATH = '/~/media/03%20Senators%20and%20Members/Address%20Labels%20and%20CSV%20files/SurnameRepsCSV.csv'
-  CSV_URL = "http://#{CSV_HOST}#{CSV_PATH}"
+  MP_CSV_PATH = '/~/media/03%20Senators%20and%20Members/Address%20Labels%20and%20CSV%20files/SurnameRepsCSV.csv'
+  SENATOR_CSV_PATH = '/~/media/03%20Senators%20and%20Members/Address%20Labels%20and%20CSV%20files/allsenel.csv'
+  MP_CSV_URL = "http://#{CSV_HOST}#{MP_CSV_PATH}"
+  SENATOR_CSV_URL = "http://#{CSV_HOST}#{SENATOR_CSV_PATH}"
 
   def scrape
+    scrape_csv(MP_CSV_URL, false).merge(scrape_csv(SENATOR_CSV_URL, true))
+  end
+
+  def scrape_csv(url, senators)
     records = {}
-    csv = CSV.read(open(CSV_URL), :headers => :true)
-    @headers = csv.headers
+    csv = CSV.read(open(url), :headers => :true)
+    headers = csv.headers
     csv.each do |line|
-      electorate, record = parse_record(line)
-      records[electorate] = record
+      key, record = parse_record(line, headers, senators)
+      records[key] = record
     end
     records
   end
 
 private
 
-  def parse_record(row)
+  def parse_record(row, headers, senator)
     record = {}
-    electorate = row['"Electorate"']
-    @headers.each_with_index do |header, index|
+    if senator 
+      key = "#{row['State']}.#{row['Surname']}.#{row['First Name']}" 
+      record[:type] = 'senator'
+    else
+      key = row['"Electorate"']
+      record[:type] = 'mp'
+    end
+    headers.each_with_index do |header, index|
       record[header.gsub('"', '').gsub(' ','_').downcase.to_sym] = row[index]
     end
 
-    [electorate, record]
+    [key, record]
   end
 end
--- spec/csv_scraper_spec.rb
+++ spec/csv_scraper_spec.rb
@@ -2,25 +2,31 @@ require_relative '../csv_scraper'
 require 'open-uri'
 
 describe CsvScraper do
-  it "can download the CSV file" do
+  it "can download the CSV files" do
     VCR.turned_off do
       WebMock.allow_net_connect!
-      Net::HTTP.start(CsvScraper::CSV_HOST, 80) {|http| expect(http.head(CsvScraper::CSV_PATH).code).to eq "200" }
+      Net::HTTP.start(CsvScraper::CSV_HOST, 80) {|http| expect(http.head(CsvScraper::MP_CSV_PATH).code).to eq "200" }
+      Net::HTTP.start(CsvScraper::CSV_HOST, 80) {|http| expect(http.head(CsvScraper::SENATOR_CSV_PATH).code).to eq "200" }
     end
   end
 
   describe "#scrape", :vcr do
-    it "scrapes MPs details correctly" do
+    it "scrapes details correctly" do
       records = subject.scrape
       curtin = records["Curtin"]
       expect(curtin.to_h).to eq CURTIN_RECORD
       durack = records["Durack"]
       expect(durack.to_h).to eq DURACK_RECORD
+      xenophon = records["SA.Xenophon.Nicholas"]
+      expect(xenophon.to_h).to eq XENOPHON_RECORD
+      parry = records["Tas.Parry.Stephen"]
+      expect(parry.to_h).to eq PARRY_RECORD
     end
 
-    it "scrapes the right number of MPs" do
+    it "scrapes the right number of MPs and senators" do
       records = subject.scrape
-      expect(records.size).to eq 150
+      expect(records.select{ |_,v| v[:type] == 'mp'}.count).to eq 150
+      expect(records.select{ |_,v| v[:type] == 'senator'}.count).to eq 75
     end
   end
 
@@ -46,7 +52,8 @@ describe CsvScraper do
    :electorate_office_fax=>"(08) 9921 7990",
    :electorate_office_phone=>"(08) 9964 2195",
    :electorate_office_toll_free=>"",
-   :parliamentary_titles=>""
+   :parliamentary_titles=>"",
+   :type=>"mp"
   }
 
   CURTIN_RECORD = {
@@ -55,7 +62,7 @@ describe CsvScraper do
    :other_names=>"Isabel",
    :preferred_name=>"",
    :initials=>"J. I.",
-   :courtesy_title=>"The Hon",
+   :courtesy_title=>"Hon",
    :salutation=>"Ms",
    :honorific=>"MP",
    :gender=>"FEMALE",
@@ -71,6 +78,61 @@ describe CsvScraper do
    :electorate_office_fax=>"(08) 9388 0299",
    :electorate_office_phone=>"(08) 9388 0288",
    :electorate_office_toll_free=>"",
-   :parliamentary_titles=>"Minister for Foreign Affairs"
+   :parliamentary_titles=>"Minister for Foreign Affairs",
+   :type=>"mp"
   }
+
+  XENOPHON_RECORD = 
+  {:type=>"senator",
+   :title=>"Senator",
+   :surname=>"Xenophon",
+   :first_name=>"Nicholas",
+   :other_names=>nil,
+   :prefered_name=>"Nick",
+   :initials=>"N.",
+   :honorifics=>nil,
+   :salutation=>"Senator",
+   :gender=>"MALE",
+   :political_party=>"Ind.",
+   :state=>"SA",
+   :electorate_addressline1=>"Level 2",
+   :electorate_addressline2=>"31 Ebenezer Place",
+   :electorate_suburb=>"Adelaide",
+   :electorate_state=>"SA",
+   :electorate_postcode=>"5000",
+   :label_address=>"Level 2, 31 Ebenezer Place",
+   :label_suburb=>"Adelaide",
+   :label_state=>"SA",
+   :label_postcode=>"5000",
+   :electorate_fax=>"(08) 8232 3744",
+   :electorate_telephone=>"(08) 8232 1144",
+   :electorate_toll_free=>"1300 556 115",
+   :parliamentary_titles=>nil}
+   
+   PARRY_RECORD =
+  {:type=>"senator",
+   :title=>"Senator the Hon",
+   :surname=>"Parry",
+   :first_name=>"Stephen",
+   :other_names=>nil,
+   :prefered_name=>"Stephen",
+   :initials=>"S.",
+   :honorifics=>nil,
+   :salutation=>"Senator",
+   :gender=>"MALE",
+   :political_party=>"LP",
+   :state=>"Tas",
+   :electorate_addressline1=>"33 George Street",
+   :electorate_addressline2=>nil,
+   :electorate_suburb=>"Launceston",
+   :electorate_state=>"Tas",
+   :electorate_postcode=>"7250",
+   :label_address=>"33 George Street",
+   :label_suburb=>"Launceston",
+   :label_state=>"Tas",
+   :label_postcode=>"7250",
+   :electorate_fax=>"(03) 6334 1624",
+   :electorate_telephone=>"(03) 6334 1755",
+   :electorate_toll_free=>"1300 760 788",
+   :parliamentary_titles=>"President of the Senate"}
 end
