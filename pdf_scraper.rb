173660b86d927d29c3370db3b1e417c415010f15
richard <richard@getup.org.au>
2015-05-24 15:36:51 +1000

    updated tests data and docs

--- pdf_scraper.rb
+++ pdf_scraper.rb
@@ -1,13 +1,15 @@
 require 'pdf-reader'
 require 'open-uri'
+require './logging.rb'
 
-#get MP contact details
-class PdfScraper
+#get MP emails
+class PdfScraper < Logging
   PDF_HOST = 'www.aph.gov.au'
   MP_PATH = '/~/media/03%20Senators%20and%20Members/32%20Members/Lists/MemList.pdf'
   MP_URL = "http://#{PDF_HOST}#{MP_PATH}"
   SENATOR_PATH = '/~/media/03%20Senators%20and%20Members/31%20Senators/contacts/los.pdf'
   SENATOR_URL = "http://#{PDF_HOST}#{SENATOR_PATH}"
+
 
   def scrape
     scrape_pdf(MP_URL, :representatives).merge(scrape_pdf(SENATOR_URL, :senate))
@@ -27,16 +29,49 @@ private
   MP_EMAIL_START_COL = 67
   MP_ELECTORATE_START_COL = 37
 
+  SENATOR_EMAIL_START_COL = 73
+  SENATOR_STATE_START_COL = 53
+
+  def new_senator_line?(line)
+    return false if line == nil || line.length < SENATOR_STATE_START_COL
+    line =~ /^\**\d+\s{3,}/ && line[5..6] == '  '
+  end
+
+  def read_senator_key(line)
+    state = line[SENATOR_STATE_START_COL..-1].split(' ')[0].downcase
+    state = line[SENATOR_STATE_START_COL..-1].split(' ')[1].downcase if state.index(')')
+    words = line[7..-1].split(' ').map {|w| w.downcase }
+    return nil if words.length < 3
+    last_name = words[0].chomp(',')
+    first_name = nil
+    if words.length > 3 && words[2] == 'the' && words[3] == 'hon'
+      first_name = words[4]
+    else
+      first_name = words[2]
+    end
+
+    "#{state}.#{last_name} #{first_name}"
+  end
+
   def read_senator_data(lines)
     records = {}
-    line_buffer = []
+    senator_key = nil
     lines.each_with_index do |line, index|
-      if email = get_email(line)
-        electorate = find_electorate(line_buffer)
-        records[electorate] = {email: email} if electorate
-        line_buffer = []
-      else
-        line_buffer << line
+      if new_senator_line?(line)
+        @logger.warn("Detected new senator but previous senator's email not recoreded: #{senator_key}") if senator_key
+        senator_key = read_senator_key(line) 
+        @logger.debug("New senator key: #{senator_key}")
+      end
+
+      if read_email('Email:', SENATOR_EMAIL_START_COL, line)
+        email = read_email('Email:', SENATOR_EMAIL_START_COL, line)
+        if senator_key
+          records[senator_key] = {email: email, house: :senate}
+          @logger.debug("Added senator: #{senator_key} => #{email}")
+        else
+          @logger.warn("Detected email but senator is not known: #{email}")
+        end
+        senator_key = nil
       end
     end
     records
@@ -46,9 +81,9 @@ private
     records = {}
     line_buffer = []
     lines.each_with_index do |line, index|
-      if email = get_email(line)
+      if email = read_email('E-mail:', MP_EMAIL_START_COL, line)
         electorate = find_electorate(line_buffer)
-        records[electorate] = {email: email} if electorate
+        records[electorate] = {email: email, house: :representatives} if electorate
         line_buffer = []
       else
         line_buffer << line
@@ -57,11 +92,13 @@ private
     records
   end
 
-  def get_email(line)
-    false unless line
-    line = line[MP_EMAIL_START_COL..-1]
-    if line && line.strip.start_with?('E-mail:')
-      line.strip.gsub('E-mail:', '').strip
+  def read_email(email_token, start_col, line)
+    return nil unless line
+    line = line[start_col..-1]
+    if line && line.strip.start_with?(email_token)
+      email = line.gsub(email_token, '').strip
+      email = email[0,email.index(' ')] if email.index(' ')
+      email
     else
       nil
     end
--- spec/pdf_scraper_spec.rb
+++ spec/pdf_scraper_spec.rb
@@ -1,27 +1,48 @@
 require_relative '../pdf_scraper'
 require 'open-uri'
 
-describe PdfScraper do
+describe PdfScraper, focus: true do
   it "can download the PDF file" do
     VCR.turned_off do
       WebMock.allow_net_connect!
-      Net::HTTP.start(PdfScraper::PDF_HOST, 80) {|http| expect(http.head(PdfScraper::PDF_PATH).code).to eq "200" }
+      Net::HTTP.start(PdfScraper::PDF_HOST, 80) {|http| expect(http.head(PdfScraper::MP_URL).code).to eq "200" }
+      Net::HTTP.start(PdfScraper::PDF_HOST, 80) {|http| expect(http.head(PdfScraper::SENATOR_URL).code).to eq "200" }
     end
   end
 
-  describe "#scrape", :vcr do
-    it "scrapes details correctly" do
-      records = subject.scrape
-      expect(records["Curtin"]).to eq({email: 'Julie.Bishop.MP@aph.gov.au'})
-      expect(records["Durack"]).to eq({email: 'Melissa.Price.MP@aph.gov.au'})
+  describe "#scrape_pdf", :vcr do
+    it "scrapes MP details correctly" do
+      records = subject.scrape_pdf(PdfScraper::MP_URL, :representatives)
+      expect(records["Curtin"][:email]).to eq('Julie.Bishop.MP@aph.gov.au')
+      expect(records["Durack"][:email]).to eq('Melissa.Price.MP@aph.gov.au')
 
-      #TODO: test senator data
+      expect(records.count{|_,v| v[:house] == :representatives}).to eq 148
     end
 
-    #this gets everything except tony abbott and clive palmer
-    it "scrapes the right number of MPs" do
-      records = subject.scrape
-      expect(records.size).to eq 148
+    it "scrapes senator details corectly" do
+      records = subject.scrape_pdf(PdfScraper::SENATOR_URL, :senate)
+      expect(records.count{|_,v| v[:house] == :senate}).to eq 75
     end
   end
+
+  describe "#new_senator_line?" do
+    specify { expect(subject.send(:new_senator_line?, "4      Bilyk, Senator CatrynaLouise                  TAS        ALP      Suite 3, Kingston Plaza, 20 Channel Highway,      (03) 6229 4444 ")).to be_truthy }
+    specify { expect(subject.send(:new_senator_line?, "**34   Lindgren, Senator JoannaMaria                 QLD         LP       2166 Logan Road, Upper Mount Gravatt QLD 4122    (07) 3422 1990 ")).to be_truthy }
+    specify { expect(subject.send(:new_senator_line?, " 44 Parliament                                                                                                             as at 21 May 2015 ")).to be_falsey }
+    specify { expect(subject.send(:new_senator_line?, " 3    Chosen by the Australian Capital Territory Legislative Assemblya casual vacancy(vice K. Lundy), pursuant to section 15 of the Constitution.  ")).to be_falsey }
+  end
+
+  describe "#read_senator_key" do
+    it "should create senator keys" do
+      line = '2      Back, Senator Christopher John (Chris)        WA          LP      Unit E5, 817 Beeliar Drive,                       (08) 9414 7288'
+      expect(subject.send(:read_senator_key, line)).to eq 'wa.back christopher'
+    end
+
+    it "should read senators with title 'the hon'" do
+      line = '1      Abetz, Senator the Hon Eric                   TAS         LP      Highbury House, 136 Davey Street,                 (03) 6224 3707'
+      expect(subject.send(:read_senator_key, line)).to eq 'tas.abetz eric'
+    end
+
+  end
+  
 end
