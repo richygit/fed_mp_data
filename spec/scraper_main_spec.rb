882d62e833db2d52b37628290e55754e69a08928
richygit <richard@getup.org.au>
2015-07-18 23:48:44 +1000

    fixed test

--- spec/scraper_main_spec.rb
+++ spec/scraper_main_spec.rb
@@ -9,23 +9,25 @@ RSpec.describe ScraperMain do
 
   describe "#main" do
     before(:each) do
-      allow_any_instance_of(CsvScraper).to receive(:scrape).and_return({grayndler: {'first_name' => 'Anthony', 'last_name' => 'Albanese'} })
-      allow_any_instance_of(WebScraper).to receive(:scrape_mps).and_return({grayndler: {'twitter' => 'http://twitter.com/AlboMP', electorate: 'Grayndler'} })
-      allow_any_instance_of(PdfScraper).to receive(:scrape).and_return({grayndler: {email: 'A.Albanese.MP@aph.gov.au'} })
+      allow_any_instance_of(CsvScraper).to receive(:scrape).and_return({durack: DURACK_RECORD})
+      allow_any_instance_of(PdfScraper).to receive(:scrape).and_return({durack: {email: 'Melissa.Price.MP@aph.gov.au'} })
     end
 
     it "should merge data from each scraper" do
       subject.main
-      grayndler = ScraperWiki::select('* FROM data WHERE electorate = "Grayndler"')
-      expect(grayndler.first).to eq({"first_name"=>"Anthony", "last_name"=>"Albanese", "electorate"=>"Grayndler", "email"=>"A.Albanese.MP@aph.gov.au"})
+      grayndler = ScraperWiki::select('* FROM data WHERE electorate = "Durack"')
+      expect(grayndler.first).to eq({"surname"=>"Price", "first_name"=>"Melissa", "other_names"=>"Lee", "preferred_name"=>"", "initials"=>"M. L.", "courtesy_title"=>"Ms", "salutation"=>"Ms", "honorific"=>"MP", "gender"=>"FEMALE", "parliament_house_telephone"=>"(02) 6277 4242", "parliament_house_fax"=>"(02) 6277 8554", "political_party"=>"LP", "state"=>"WA", "electorate"=>"Durack", "electorate_office_postal_address"=>"2B/209 Foreshore Drive", "electorate_office_postal_suburb"=>"Geraldton", "electorate_office_postal_state"=>"WA", "electorate_office_postal_postcode"=>"6530", "electorate_office_fax"=>"(08) 9921 7990", "electorate_office_phone"=>"(08) 9964 2195", "electorate_office_toll_free"=>"", "parliamentary_titles"=>"", "type"=>"mp", "email"=>"Melissa.Price.MP@aph.gov.au"})
     end
   end
 
-  describe "full test", :vcr, focus: true do
+  describe "full test", :vcr do
     it "should merge all data" do
       subject.main
       records = ScraperWiki::select('* FROM data')
       expect(records.count).to eq (150+77)
     end
   end
+  
+
+  DURACK_RECORD = {"surname"=>"Price", "first_name"=>"Melissa", "other_names"=>"Lee", "preferred_name"=>"", "initials"=>"M. L.", "courtesy_title"=>"Ms", "salutation"=>"Ms", "honorific"=>"MP", "gender"=>"FEMALE", "parliament_house_telephone"=>"(02) 6277 4242", "parliament_house_fax"=>"(02) 6277 8554", "political_party"=>"LP", "state"=>"WA", "electorate"=>"Durack", "electorate_office_postal_address"=>"2B/209 Foreshore Drive", "electorate_office_postal_suburb"=>"Geraldton", "electorate_office_postal_state"=>"WA", "electorate_office_postal_postcode"=>"6530", "electorate_office_fax"=>"(08) 9921 7990", "electorate_office_phone"=>"(08) 9964 2195", "electorate_office_toll_free"=>"", "parliamentary_titles"=>"", 'type'=>'mp'}
 end
